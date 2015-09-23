8b67407dbe287529353a8f09402d018b92212fb6
squidwardT <lavigne.de@husky.neu.edu>
2015-09-23 18:05:36 -0400

    Docs Updated

--- batman_sockets/BatmanClientServerSocket.py
+++ batman_sockets/BatmanClientServerSocket.py
@@ -3,28 +3,69 @@ from batman_socket import BatmanSocket
 from BatmanServerSocket import BatmanServerSocket
 
 class BatmanClientServerSocket(BatmanServerSocket):
+	'''CLASS: Representing a BATMAN-Advanced node capable of listening, interpreting
+	          actions, and transmitting data.
+
+	FIELDS:
+	@transmission_client		-- A socket capable of sending out data to other nodes.
+	'''
 	def __init__(self, address, client = None):
 		super(BatmanClientServerSocket, self_.__init__('idc')
+		# Initialize transmission client
 		if client is None:
 			self.transmission_client = BatmanSocket()
 		else:
 			self.transmission_client = client
 
 	def replace_client(self):
+		'''Create new transmission client.'''
 		self.transmission_client = BatmanSocket()
 
-	def read_client(self, client):
+	def read_client(self, client, address):
+		'''Get the arguments from the server socket and interpret them.
+
+		ARGS:
+		@client 		-- The client socket to read from.
+		@address		-- The address of the client socket (for returning data)
+
+		RETURNS:
+		None
+		'''
+
 		args = BatmanServerSocket.read_client(client)
 		self.interpret_args(args, client)
 
-	def interpret_args(self, args, client):
-		if args[0] == 'ping':
-			(stdout, stderr) = run_command('sudo batctl ping ' + args[1])
-		elif args[0] == 'traceroute':
-			(stdout, stderrr) = run_command('sudo batctl tracerout ' + args[1])
-		return stdout
+	def interpret_args(self, args, client, address):
+		'''Interpret the arguments received from another device and do what they ask.
 
-	def client_send_message(host, message, port = 56634):
+		ARGS:
+		@args 			-- The list of arguments to execute.
+		@client 		-- The client the data came from.
+		@address		-- The client's return address.
+
+		RETURNS:
+		None
+		'''
+		if args[0] == 'ping':
+			(stdout, stderr) = run_command('sudo batctl ping -c 1' + args[1])
+			xml_ping = xml_parser.parse_ping_to_xml(stdout)
+			client_send_message(address, 'ping_response ' + open(xml_ping, 'r').read())
+		elif args[0] == 'traceroute':
+			(stdout, stderrr) = run_command('sudo batctl traceroute ' + args[1])
+			xml_traceroute = xml_parser.parse_traceroute_to_xml(stdout)
+			client_send_message(address, 'traceroute_response ' + open(xml_traceroute, 'r').read())
+
+	def client_send_message(self, host, message, port = 56634):
+		'''Send a message.
+
+		ARGS:
+		@host			-- The host to send data to.
+		@message 		-- The data to send
+		@port 			-- The port the application is listening on.
+
+		RETURNS:
+		None
+		'''
 		self.transmission_client.connect(host, port)
 		self.transmission_client.write(message)
 		self.replace_client()
--- batman_sockets/BatmanServerSocket.py
+++ batman_sockets/BatmanServerSocket.py
@@ -31,56 +31,79 @@ class BatmanServerSocket(BatmanSocket):
 		None		-- Theoretically Endless
 		'''
 
+		# Bind this socket to port and listen for connections
 		self.sock.bind((socket.gethostname(), port))
 		self.sock.listen(5)
 
+		# While the application is running attempt listen for connections and read the
+		# connecting devices message.
 		while True:
+			# Get the connected socket and make a Batman Socket out of it
 			client, address = self.sock.accept()
 			batman_client = BatmanSocket(address, client, address)
 
+			# Start threads to add devices to the list of clients and read their messages
 			add_thread = Thread(target = self.add_client, args = [batman_client])
 			read_thread = Thread(target = self.read_client, args = [batman_client])
 			add_thread.start()
 			read_thread.start()
 
-	def update_client(self, client):
+	def update_client(self):
+		'''Update the list of known clients removing all clients that are not readable
+		   or writable or are in error.
+
+		ARGS:
+		None
+
+		RETURNS:
+		None
+		'''
+		# Check the states of clients
 		ready_to_read, ready_to_write, in_error = select.select(self.clients,
 												  self.clients, self.clients, 60.0)
+		# Remove items in error
 		for item in in_error:
 			self.clients.remove(item)
-
+		# Remove items not readable or writable
+		for item in self.clients:
+			if item not in ready_to_read and not in ready_to_write:
+				self.clients.remove(item)
+		# Read ready to read clients
 		for item in ready_to_read:
 			self.read_client(item)
 
-		for item in self.clients:
-			if item.address == client.address:
-				self.clients.remove(user)
-
-
 	def add_client(self, clients):
-		'''Add a client '''
+		'''Add a client to the list of recent client. If it already exists update it.
 
+		ARGS:
+		@clients		-- The client to be added to the list
+
+		RETURNS:
+		None
+		'''
+		# Update client list and read out inbound messages
 		client = clients
 		self.update_client(client)
+
+		# If the newly connected device is already in list update it otherwise add it
+		for item in self.clients:
+			if item.address == client.address:
+				self.clients.remove(item)
 		self.clients.append(client)
 			
 
 	def read_client(self, clients, address = None):
-		client = clients
-		message = client.read()
-		args = shlex.split(message)
+		'''Receive the client's message.
 
-		out = []
-		if 'g' in args[0]:
-			for arg in args[1 : ]:
-				response = client.get(arg)
-				out.append(response)
-		if 'p' in args[0]:
-			for arg in args[1 : ]:
-				client.post(arg)
-		if 'd' in args[0]:
-			for arg in args[1 : ]:
-				client.display(arg)
+		ARGS:
+		@clients		-- The client socket to be read from.
+		@address		-- IGNORE. Purely for error prevention
+
+		RETURNS:
+		@args 			-- A list of arguments to be interpreted
+		'''
+		return shlex.split(clients.read())
+		
 
 if __name__ == '__main__':
 	server = BatmanServerSocket()
