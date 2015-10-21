f6c4a367f4a821834264744d63e9d68418b533d9
squidwardT <lavigne.de@husky.neu.edu>
2015-10-21 13:28:42 -0400

    posting to server works

--- server_communication/post_resource_to_server.py
+++ server_communication/post_resource_to_server.py
@@ -12,29 +12,44 @@ def post_resource_to_server(rsc_path, payload):
 	'''
 	import json
 	import requests
+	from BeautifulSoup import BeautifulSoup
 
 	# Build the path to the resource
 	abs_path = 'http://batphone.co:3000/' + rsc_path
 	print 'Posting to ' + abs_path
 
-	# POST the data
-	request = requests.post(abs_path, data=payload)
+	# GET an authenticity token 
+	session = requests.Session()
+	html = BeautifulSoup(session.get('http://batphone.co:3000/login').text)
+	csrf = html.head.find('meta', attrs = {'name' : 'csrf-token'})['content']
+	payload['authenticity_token'] = csrf
+
+	# Define a JSON header
+	header = {'content-type' : 'application/json'}
 	
+	# Post data in JSON format with the JSON header
+	response = session.post(abs_path, 
+		 					headers = header, 
+		 					data = json.dumps(payload))
+
 	# Check whether the POST was successful
-	if request.status_code == '200':
-		return request
+	if session.status_code == 200:
+		print 'Success'
+		return session
+	else: print str(session.status_code)
 	return False
 
 if __name__ == '__main__':
 	import argparse
 	parser = argparse.ArgumentParser()
 	parser.add_argument('path')
-	#parser.add_argument('payload')
+	parser.add_argument('payload')
 	args = parser.parse_args()
 
-	new_user_info = { 'name' : 'squid',
-					  'email' : 'sqt@hotmail.com',
-					  'password' : 'helloworld',
-					  'password_confirmation' : 'helloworld'}
-
-	post_resource_to_server(args.path, { 'user' : new_user_info })
+	path = 'users'
+	data = {'user' : {'name' : 'squid',
+			'email': 'sqt@hotmail.com',
+			'password' : 'helloworld',
+			'password_confirmation' : 'helloworld'},
+			'commit' : 'Create my account'}
+	post_resource_to_server(args.path, args.payload)
